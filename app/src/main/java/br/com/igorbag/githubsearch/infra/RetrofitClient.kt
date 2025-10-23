1ad667a4814ad4dd81446bc11137c9ee4545021e
Patrício Souza <>
2025-10-23 17:28:56 -0300

    Implementação da lista de TODOs para entrega do desafio.

---
+++ app/src/main/java/br/com/igorbag/githubsearch/infra/RetrofitClient.kt
@@ -0,0 +1,30 @@
+package br.com.igorbag.githubsearch.infra
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+object RetrofitClient {
+
+    private const val BASE_URL = "https://api.github.com/"
+
+    private val logging = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private val client = OkHttpClient.Builder()
+        .connectTimeout(30, TimeUnit.SECONDS)
+        .readTimeout(30, TimeUnit.SECONDS)
+        .addInterceptor(logging)
+        .build()
+
+    val instance: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+    }
+}
